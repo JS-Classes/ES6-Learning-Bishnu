70f6f710f77e7a5e00e52fa92fc7f528971a743d
Bishnu Rawal <bishnu.rawal@cotiviti.com>
2020-11-26 09:19:54 +0545

    Learned about datatypes

---
+++ Variables.js
@@ -0,0 +1,28 @@
+// Primitive
+var a = "bishnu";
+var b = true;
+
+var c = 345.6;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; // not a number
+var x = "dfsdf"/34;
+// console.log(x);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+var d = 3n ** 123n;
+
+var e = undefined;
+var f = null;
+
+// Structural
+var g = ["a", "b", "c"];
+var h = { name: "keshab", age: 12 };
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+
