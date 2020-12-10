a9ad159a728d38401bc63b78fed8d28fff02d537
Bishnu Rawal <bishnu.rawal@cotiviti.com>
2020-12-10 14:01:44 +0545

    Added excercise

---
+++ Excercises.js
@@ -0,0 +1,17 @@
+// 1. Find matching numbers in a paragraph using regex?
+// 2. Validate emails using regex
+
+// 1. Program to print multiplication table for a number
+// 2. Program to print multiplication table for a number upto some range (default should be 10)
+// 3. Program to print multilication table for numbers from m to n. (use console.table)
+
+
+// 1. Declare a string variable with paragrpah as value. Display following stat:
+    // a. Number of sentences, words and word characters
+    // b. Number of special characters
+    // c. Numbers of vowels and consonants
+
+// 1. Find normal distribution of set of random numbers
+// 2. Find a random number between provided m and n
+// 3. Find a random string of particular length
+
