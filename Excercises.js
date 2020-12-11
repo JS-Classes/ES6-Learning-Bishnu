2d991e164b3cb1a1ff3c58da15500af91d5dc7c2
Bishnu Rawal <bishnu.rawal@cotiviti.com>
2020-12-11 10:13:27 +0545

    DIscussed possible solution on random string

--- Excercises.js
+++ Excercises.js
@@ -7,11 +7,34 @@
 
 
 // 1. Declare a string variable with paragrpah as value. Display following stat:
-    // a. Number of sentences, words and word characters
-    // b. Number of special characters
-    // c. Numbers of vowels and consonants
+// a. Number of sentences, words and word characters
+// b. Number of special characters
+// c. Numbers of vowels and consonants
 
 // 1. Find normal distribution of set of random numbers
 // 2. Find a random number between provided m and n
 // 3. Find a random string of particular length
+
+
+// 1. Find a random string of particular length (Using approach other than following)
+
+
+let getRandomString = (length) => {
+    const wordChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+
+    for (let i = 0; i < length; i++) {
+        let randomNum = Math.random() * wordChars.length;
+        let index = Math.floor(randomNum);
+        let c = wordChars.charAt(index);
+        randomString += c;
+    }
+
+    return randomString;
+}
+
+let randString = getRandomString(20);
+
+console.log(randString);
+
 
